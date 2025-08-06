50b7eec258841ade7c6868d7cced5292d8cfb6c6
beyzza <beyzkrc.0@gmail.com>
2025-08-06 18:33:34 +0300

    Color dosyasına renkler eklendi.

--- app/src/main/java/com/example/userlistapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/userlistapp/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Custom colors
+val PrimaryBlue = Color(0xFF2196F3)
+val SecondaryBlue = Color(0xFF03DAC6)
+val BackgroundGray = Color(0xFFF5F5F5)
+val CardWhite = Color(0xFFFFFFFF)
+val ErrorRed = Color(0xFFB00020)
